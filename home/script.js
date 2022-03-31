d4d4a2fa7dbe450d0e92fc8bc1ceba03e782628e
Elijah Erickson <elijahrerickson@gmail.com>
2022-03-30 20:43:17 -0500

    Create game and graphics.
    Still working on figuring out animation on keypress, it pushes other
    elements, maybe make divs absolute inside of a relative container?

--- home/script.js
+++ home/script.js
@@ -1,43 +1,111 @@
 const game = document.getElementById('game');
-const arr = [];
-const solved = ['h', 'e', 'a', 'r', 't'];
-let c = 0;
+const arrHolder = document.createElement('array');
+const page = document.getElementById('grid-container');
+const end = document.getElementById('end');
+const endWin = document.getElementById('win');
+const endLose = document.getElementById('lose');
 
-for (let i = 0; i < 5; i++){
-    arr[i] = document.createElement('div');
-    arr[i].classList.add('letter-box');
-    game.appendChild(arr[i]);
+const solved = ['s', 'n', 'a', 'c', 'k'];
+let c = 0;
+let u = 0;
+
+for (let i = 0; i < 6; i++){
+    arrHolder[i] = document.createElement('div');
+    arrHolder[i].classList.add('row');
+    for (let j = 0; j < 5; j++){
+        const arr = [];
+        arr[j] = document.createElement('div');
+        arr[j].classList.add('letter-box');
+        arrHolder[i].appendChild(arr[j]);
+    }
+    game.appendChild(arrHolder[i]);
 }
 
 window.addEventListener('keydown', keydown);
 
 function keydown(keyCode){    
     const key = keyCode.key;
-    if (key == 'Backspace'){
-        c--;
-        arr[c].textContent = '';
+    const val = keyCode.keyCode;
+    console.log(val);
+    
+    switch(true){
+        case (val == 13): //enter
+        case (val == 8): //backspace
+            break;
+
+        case (val < 65):
+        case (val > 90):
+            return;
     }
 
-    if (key == 'Enter' && c==5){
+    
+
+    if (key == 'Backspace' && c>0){
+        c--;
+        arrHolder[u].children[c].textContent = '';
+    }
+
+    let correctCount = 0;
+    if (key == 'Enter' && c>=5){
+        let exactCount = 0;
+        console.log(keyCode);
+
         for (c = 4; c >= 0; c--){
+            let correct = false;
             for (let j = 0; j < 5; j++){
-                if (arr[c].childNodes[0].innerHTML == solved[j]){
+                if (arrHolder[u].childNodes[c].firstChild.innerHTML.toLowerCase() == solved[j]){
                     if (j == c){
-                        arr[c].classList.add('fully-guessed');
+                        arrHolder[u].childNodes[c].classList.add('fully-guessed');
+                        correct = true;
+                        exactCount++;
                     }else{
-                        arr[c].classList.add('partial-guessed');
+                        arrHolder[u].childNodes[c].classList.add('partial-guessed');
+                        correct = true;
                     }
                 } 
             }
+            if (correct == false){
+                arrHolder[u].childNodes[c].classList.add('not-in-word');
+            }else{
+                correctCount++;
+            }
         }
-
+        
+        if (exactCount == 5){
+            win();
+        }
+        if (exactCount < 5 && u==5){
+            lose();
+        }
+        c=0;
+        u++;
     }
 
     if (key != 'Backspace' && key != 'Enter'){
         const letter = document.createElement('h1');
         letter.classList.add('letter')
-        letter.textContent=key;
-        arr[c].appendChild(letter);
+        letter.textContent=key.toUpperCase();
+        arrHolder[u].children[c].appendChild(letter);
+        arrHolder[u].childNodes[c].classList.add('newLetter');
+
         c++;
     }
+}
+
+function win(){
+    window.removeEventListener('keydown', keydown);
+    end.classList.toggle('hidden');
+    let thing = document.createElement('h1');
+    thing.textContent = 'It took ' + (u+1) + ' tries.';
+    endWin.appendChild(thing);
+    endWin.classList.toggle('hidden');
+}
+
+function lose(){
+    window.removeEventListener('keydown', keydown);
+    end.classList.toggle('hidden');
+    page.classList.add('lose');
+    endLose.classList.toggle('hidden');
+
+
 }
